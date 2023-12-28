43a58964df7876cdc07b31c45025b308f002e5c1
Madara Uchiha <me@madara.ninja>
2023-12-29 01:25:45 +0200

    cache TryGetQuality

--- Source/Patches.cs
+++ Source/Patches.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using HarmonyLib;
 using RimWorld;
 using Verse;
@@ -7,16 +8,31 @@ namespace ShelfQualityMatters
     [HarmonyPatch(typeof(Building), "get_MaxItemsInCell")]
     public class Patches
     {
+        private static readonly Dictionary<int, QualityCategory> thingIdToQualityCache = new Dictionary<int, QualityCategory>();
         public static bool Prefix(Building __instance, ref int __result)
         {
-            if (!(__instance is Building_Storage)) {
-                return true;
-            }
-            if (!__instance.TryGetQuality(out var qc))
+            // Don't care about nonstorage buildings
+            if (!(__instance is Building_Storage))
             {
                 return true;
             }
+
+            // cached?
+            if (thingIdToQualityCache.TryGetValue(__instance.thingIDNumber, out var value))
+            {
+                __result = QualityToStorage(value);
+                return false;
+            }
+
+            // not cached, try the expensive call
+            if (!__instance.TryGetQuality(out var qc))
+            {
+                // item has no quality for some reason, resume original function call
+                return true;
+            }
+
             __result = QualityToStorage(qc);
+            thingIdToQualityCache[__instance.thingIDNumber] = qc;
             return false;
         }
         private static int QualityToStorage(QualityCategory quality)
